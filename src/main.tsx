cc3b60837a60496ead5c8172c8165322a5eb167f
kzau-1612 <kzau1612@gmail.com>
2025-05-13 16:30:08 +0700

    add libs

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,20 @@ import "./index.css";
 import "@mantine/core/styles.css";
 import "normalize.css";
 import { MantineProvider, createTheme } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // retry: 5,
+      retry: 1,
+      // retryDelay: 1000,
+      refetchOnWindowFocus: false,
+      gcTime: 600000,
+      refetchOnReconnect: false,
+    },
+  },
+});
 
 const theme = createTheme({
   /** Your theme override here */
@@ -12,8 +26,10 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <MantineProvider theme={theme}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </QueryClientProvider>
   </MantineProvider>
 );
