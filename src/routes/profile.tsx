4048ee80528af64f20be65887ef6f64d0ed27811
kzau-1612 <kzau1612@gmail.com>
2025-05-16 21:59:32 +0700

    blocker

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,9 +1,30 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useBlocker } from "@tanstack/react-router";
+import { useState } from "react";
 
 export const Route = createFileRoute("/profile")({
   component: Profile,
 });
 
 function Profile() {
-  return <div>Hello "/profile"!</div>;
+  const [name, setName] = useState("");
+  const { proceed, reset, status } = useBlocker({
+    shouldBlockFn: () => !!name,
+    enableBeforeUnload: !!name,
+    withResolver: true,
+  });
+
+  console.log(status);
+
+  return (
+    <div>
+      <h2>Hello "/profile"!</h2>
+      {status === "blocked" && (
+        <>
+          <button onClick={proceed}>Sure</button>
+          <button onClick={reset}>cancel</button>
+        </>
+      )}
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+    </div>
+  );
 }
