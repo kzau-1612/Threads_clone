64f329cb497db3c4ee7b254190bc025e953de09d
kzau-1612 <kzau1612@gmail.com>
2025-06-04 18:07:53 +0700

    Chỉnh style và global style

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import "normalize.css";
 import "@mantine/core/styles.css";
+import styles from "./index.module.css";
 import App from "./App.tsx";
-import { MantineProvider, createTheme } from "@mantine/core";
+import { Input, MantineProvider, createTheme } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { store } from "./stores/store.ts";
@@ -25,7 +25,11 @@ const queryClient = new QueryClient({
 const theme = createTheme({
   /** Your theme override here */
   radius: { xs: "12px", sm: "12px", md: "12px", lg: "12px", xl: "12px" },
-  // fontFamily: "system-ui,-apple-system,BlinkMacSystemFont,Roboto,Helvetica,,sans-serif",
+  components: {
+    Input: Input.extend({ classNames: styles }),
+  },
+
+  fontFamily: "Helvetica, Arial, sans-serif",
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
--- src/routes/(auth-layout)/_auth-layout/(login)/login.tsx
+++ src/routes/(auth-layout)/_auth-layout/(login)/login.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { isAuthenticated, signIn, signOut } from "../../../../utils/auth";
-import { PasswordInput, TextInput } from "@mantine/core";
+import { Button, PasswordInput, TextInput } from "@mantine/core";
 import styles from "./login.module.css";
 
 export const Route = createFileRoute("/(auth-layout)/_auth-layout/(login)/login")({
@@ -18,8 +18,19 @@ function Login() {
   return (
     <form>
       <span className={styles.title}>Đăng nhập với tài khoản Instagram</span>
-      <TextInput placeholder="Tên người dùng, số điện thoại hoặc email" className={styles.input} />
-      <PasswordInput placeholder="Mật khẩu" className={styles.input} size={"md"} mt={"md"} />
+      <TextInput
+        placeholder="Tên người dùng, số điện thoại hoặc email"
+        classNames={{ input: styles.input }}
+        size="lg"
+      />
+      <PasswordInput
+        placeholder="Mật khẩu"
+        classNames={{ input: styles.input, innerInput: styles.innerInput }}
+        size="lg"
+      />
+      <Button variant="filled" color="black" className={styles.button} size={"md"} mt={"md"}>
+        Đăng nhập
+      </Button>
     </form>
   );
 }
