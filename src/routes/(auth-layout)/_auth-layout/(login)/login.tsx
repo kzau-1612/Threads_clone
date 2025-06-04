237d732224914e7bffbc0d3cc7c8f17dd9fc95b1
kzau-1612 <kzau1612@gmail.com>
2025-06-04 23:53:48 +0700

    thêm bottom

--- src/routes/(auth-layout)/_auth-layout/(login)/login.tsx
+++ src/routes/(auth-layout)/_auth-layout/(login)/login.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { isAuthenticated, signIn, signOut } from "../../../../utils/auth";
-import { Button, PasswordInput, TextInput } from "@mantine/core";
+import { Anchor, Button, PasswordInput, TextInput } from "@mantine/core";
 import styles from "./login.module.css";
 
 export const Route = createFileRoute("/(auth-layout)/_auth-layout/(login)/login")({
@@ -16,22 +16,32 @@ function Login() {
   const router = useRouter();
   const checkLogin = Route.useLoaderData();
   return (
-    <form>
-      <span className={styles.title}>Đăng nhập với tài khoản Instagram</span>
-      <TextInput
-        placeholder="Tên người dùng, số điện thoại hoặc email"
-        classNames={{ input: styles.input }}
-        size="lg"
-      />
-      <PasswordInput
-        placeholder="Mật khẩu"
-        classNames={{ input: styles.input, innerInput: styles.innerInput }}
-        size="lg"
-      />
-      <Button variant="filled" color="black" className={styles.button} size={"md"} mt={"md"}>
-        Đăng nhập
-      </Button>
-    </form>
+    <>
+      <form>
+        <span className={styles.title}>Đăng nhập với tài khoản Instagram</span>
+        <TextInput
+          placeholder="Tên người dùng, số điện thoại hoặc email"
+          classNames={{ input: styles.input }}
+          size="lg"
+        />
+        <PasswordInput
+          placeholder="Mật khẩu"
+          classNames={{ input: styles.input, innerInput: styles.innerInput }}
+          size="lg"
+        />
+        <Button variant="filled" color="black" className={styles.button} mt="md" size="lg" disabled>
+          Đăng nhập
+        </Button>
+      </form>
+      <div className={styles.bottom}>
+        <Anchor c="var(--mantine-color-gray-6)" size="sm" underline="never">
+          Quên mật khẩu?
+        </Anchor>
+        <div className={styles.or}>
+          <span>hoặc</span>
+        </div>
+      </div>
+    </>
   );
 }
 
